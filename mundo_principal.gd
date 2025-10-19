34d33d3e3a9cfe9a070cb329b832211a329afb06
Francisco Gasca <francisco_gasca@hotmail.com>
2025-10-19 13:42:42 -0600

    feat(ui): Agrega botón de interacción para RolMadre

--- mundo_principal.gd
+++ mundo_principal.gd
@@ -10,3 +10,14 @@ func _ready() -> void:
 
 func actualizar_etiqueta_dinero() -> void:
 	etiqueta_contador.text = "Dinero Restante: " + str(dinero_rey)
+
+
+func _on_btn_madre_pressed() -> void:
+	if dinero_rey >= 1:
+		dinero_rey -= 1
+
+		actualizar_etiqueta_dinero()
+
+		print("Se ha dado 1 moneda a madre. Dinero Restante: " + str(dinero_rey))
+	else:
+		print("No hay más dinero para repartir!")
