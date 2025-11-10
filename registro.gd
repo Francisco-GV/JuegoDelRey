ed82ad007fc98592bd0505cc159ae74c0da2b409
Francisco Gasca <francisco_gasca@hotmail.com>
2025-11-10 02:49:15 -0600

    feat(scene): Agrega botón para finalizar registro

--- registro.gd
+++ registro.gd
@@ -2,7 +2,14 @@ extends CanvasLayer
 
 @export var gender_group: ButtonGroup
 
+@onready var txt_nombre: LineEdit = $Scale1/Left/VBoxContainer/TxtNombre
+@onready var txt_edad: SpinBox = $Scale1/Left/VBoxContainer/TxtEdad
+
 @onready var selector: HFlowContainer = $Scale1/Left/VBoxContainer/GenderSelector
+@onready var btn_continuar: Button = $Scale1/Right/VBoxContainer/BtnContinuar
+
+var selected_gender: String = "Hombre"
+var selected_character: Texture
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -17,5 +24,23 @@ func _ready() -> void:
 
 func _on_gender_selected(button_pressed: BaseButton) -> void:
 	# Realmente tienen que ser de tipo GenderButton
-	print("Se presionó el botón: ", button_pressed.gender_value)
-	pass
+	selected_gender = button_pressed.gender_value
+
+
+func _on_btn_continuar_pressed():
+	var nombre: String = txt_nombre.text.strip_edges()
+
+	if nombre.is_empty():
+		return
+
+	var edad := int(txt_edad.value)
+	var genero: String = selected_gender
+	var _textura: Texture = selected_character
+
+	print("nombre:", nombre)
+	print("edad:", edad)
+	print("género:", genero)
+
+
+func _on_character_selector_character_selected(character_texture: Texture) -> void:
+	selected_character = character_texture
