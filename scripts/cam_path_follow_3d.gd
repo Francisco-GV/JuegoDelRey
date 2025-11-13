37a54806f8c2c9ba8acaea073576ac3f746bd863
Francisco Gasca <francisco_gasca@hotmail.com>
2025-11-13 04:14:44 -0600

    feat(camera): Agrega interpolación en transición de cámaras

--- scripts/cam_path_follow_3d.gd
+++ scripts/cam_path_follow_3d.gd
@@ -12,6 +12,11 @@ func _process(delta):
 
 	progress += speed * delta
 
+	if progress_ratio >= 0.95 and not finalizado:
+		var t = clamp((progress_ratio - 0.95) / 0.05, 0.0, 1.0)
+		global_position = global_position.lerp(camara_fija.global_position, t)
+		global_rotation = global_rotation.lerp(camara_fija.global_rotation, t)
+
 	if progress_ratio >= 1.0:
 		progress_ratio = 1.0
 		finalizado = true
